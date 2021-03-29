43bfe4c6ce37edf2cae42f3dc6e3469d6d23d5f0
mmillerick <michael.millerick@gmail.com>
2021-03-28 17:15:50 -0700

    last lint fix

--- src/controller/activityController.ts
+++ src/controller/activityController.ts
@@ -2,13 +2,28 @@ import * as _ from 'lodash';
 import * as utils from '../utils';
 import { ActivityData } from '../model/activityDataModel';
 
+type MetricName = 'Distance' | 'Elevation Gain' | 'Elapsed Time';
+type MetricUnit = 'Miles' | 'Feet' | 'Seconds';
+
+interface SeriesItem {
+  metric: MetricName;
+  unit: MetricUnit;
+  series: [string, number][];
+}
+
+interface AggregateItem {
+  distance: number;
+  elevationGain: number;
+  elapsedTime: number;
+}
+
 /**
  * Perform aggregations on an array of activities after filtering to a specific ActivityType
  * @param allActivities Array of activities.
  * @param activityType ActivityType to filter to.
  */
-export function aggregateActivityType(allActivities: ActivityData[]): any {
-  const activityAggregates = {};
+export function aggregateActivityType(allActivities: ActivityData[]): SeriesItem[] {
+  const activityAggregates: { [key: string]: AggregateItem } = {};
   const keys = _.reverse(
     _.sortedUniq(_.map(allActivities, (activity) => utils.getDateStringFromDate(activity.activity_date))),
   );
@@ -29,19 +44,19 @@ export function aggregateActivityType(allActivities: ActivityData[]): any {
   });
   const series = [];
   series.push({
-    metric: 'Distance',
-    unit: 'Miles',
-    series: _.map(keys, (key) => [key, activityAggregates[key].distance]),
+    metric: 'Distance' as MetricName,
+    unit: 'Miles' as MetricUnit,
+    series: _.map(keys, (key): [string, number] => [key, activityAggregates[key].distance]),
   });
   series.push({
-    metric: 'Elevation Gain',
-    unit: 'Feet',
-    series: _.map(keys, (key) => [key, activityAggregates[key].elevationGain]),
+    metric: 'Elevation Gain' as MetricName,
+    unit: 'Feet' as MetricUnit,
+    series: _.map(keys, (key): [string, number] => [key, activityAggregates[key].elevationGain]),
   });
   series.push({
-    metric: 'Elapsed Time',
-    unit: 'Seconds',
-    series: _.map(keys, (key) => [key, activityAggregates[key].elapsedTime]),
+    metric: 'Elapsed Time' as MetricName,
+    unit: 'Seconds' as MetricUnit,
+    series: _.map(keys, (key): [string, number] => [key, activityAggregates[key].elapsedTime]),
   });
   return series;
 }
